4dc21af43b778a20f2e4e84bef431cc297dbff54
Ishita <ish@Vineets-MacBook-Air.local>
2025-10-24 23:39:10 +0530

    Style: Resize header logo to 40%

--- cogni-flow/src/App.js
+++ cogni-flow/src/App.js
@@ -725,6 +725,7 @@ export default function App() {
             alt="Cogni-Flow Logo" 
             className="header-logo" 
             src={process.env.PUBLIC_URL + "/Header-logo.png"} 
+            style={{ width: '26px', height: '26px' }}
           />
           <h1 className="header-title">CogniFlow</h1>
         </div>
